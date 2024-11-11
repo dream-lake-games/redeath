6403629d613946dd35b1a1df861a6df96da27053
Mark Pekala <mfpekala@gmail.com>
2024-11-10 16:14:39 -0800

    it's taking form...

--- src/cutscene/cutscene_macros.rs
+++ src/cutscene/cutscene_macros.rs
@@ -1,4 +1,4 @@
-macro_rules! reg_basic_fns {
+macro_rules! add_basic_systems {
     ($app:expr, $variant_name:ident) => {
         $app.add_systems(OnEnter(CutsceneState::$variant_name), on_enter);
         $app.add_systems(
@@ -8,7 +8,17 @@ macro_rules! reg_basic_fns {
         $app.add_systems(OnExit(CutsceneState::$variant_name), on_exit);
     };
 }
-pub(super) use reg_basic_fns;
+pub(super) use add_basic_systems;
+
+macro_rules! add_puppet_init {
+    ($app:expr, $variant_name:ident) => {
+        $app.add_systems(
+            OnEnter(PlayerMetaState::Puppet),
+            init_puppet.run_if(in_state(CutsceneState::$variant_name)),
+        );
+    };
+}
+pub(super) use add_puppet_init;
 
 macro_rules! decl_cutscene_event {
     ($event_name:ident, $fun:item) => {
--- src/cutscene/lake_intro.rs
+++ src/cutscene/lake_intro.rs
@@ -8,6 +8,11 @@ fn on_enter(root: Res<ConvoRoot>, mut commands: Commands) {
     commands.spawn(DoInSeconds::new(StartIntroConvo, 1.0));
 }
 
+fn init_puppet(mut player_anim: Query<&mut AnimMan<PlayerAnim>>) {
+    let mut player_anim = player_anim.single_mut();
+    player_anim.set_state(PlayerAnim::EdgeSitting);
+}
+
 decl_cutscene_event!(
     StartIntroConvo,
     fn start_intro_convo(_trigger: Trigger<StartIntroConvo>, mut commands: Commands) {
@@ -20,5 +25,6 @@ fn update() {}
 fn on_exit() {}
 
 pub(super) fn register_lake_intro(app: &mut App) {
-    reg_basic_fns!(app, LakeIntro);
+    add_basic_systems!(app, LakeIntro);
+    add_puppet_init!(app, LakeIntro);
 }
